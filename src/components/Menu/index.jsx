b32d7bcd95c4f5370d7032276437b68e1b4882fc
zahra <zmohseni95@gmail.com>
2023-01-10 11:46:21 +0330

    feat: add menu

---
+++ src/components/Menu/index.jsx
@@ -0,0 +1,27 @@
+//styles
+import "./index.css";
+export function Menu() {
+  return (
+    <header className="navbar-fixed-top bg-nav">
+      <nav>
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#experience">experience</a>
+          </li>
+          <li>
+            <a href="#projects">projects</a>
+          </li>
+          <li>
+            <a href="#contact">contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
